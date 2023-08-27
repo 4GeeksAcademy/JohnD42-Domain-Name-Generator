9b0ac1643a2d701d487f93b41cde31c2bbe1f9d6
JohnD42 <139003527+JohnD42@users.noreply.github.com>
2023-08-27 07:04:06 +0000

    Maybe finished project

--- src/app.js
+++ src/app.js
@@ -1,11 +1,19 @@
 /* eslint-disable */
-import "bootstrap";
-import "./style.css";
+  let pronoun = ["the", "our", "your", "my"];
+  let adj = ["great", "big", "fabulous", "wonderful", "silly", "superb"];
+  let noun = ["jogger", "racoon", "goose", "tardigrade", "penguin", "clown"];
+  let end = [".com", ".io", ".org", ".net"];
+  let domainName = "";
+  let domainNames = [];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+  for (let i in pronoun) {
+    for (let j in adj) {
+      for (let k in noun) {
+        for (let l in end) {
+          domainName = [pronoun[i] + adj[j] + noun[k] + end[l]].join();
+          domainNames.push(domainName);
+        }
+      }
+    }
+  }
+  console.log(domainNames);

1a97df7df0728646e99b27aef431ff9a8cfef3df
JohnD42 <139003527+JohnD42@users.noreply.github.com>
2023-08-31 01:37:22 +0000

    Updated to show domain names on webpage instead of only in console. No effort given to appearance since the focus of this project is the javascript.

--- src/app.js
+++ src/app.js
@@ -1,19 +1,26 @@
 /* eslint-disable */
-  let pronoun = ["the", "our", "your", "my"];
-  let adj = ["great", "big", "fabulous", "wonderful", "silly", "superb"];
-  let noun = ["jogger", "racoon", "goose", "tardigrade", "penguin", "clown"];
-  let end = [".com", ".io", ".org", ".net"];
-  let domainName = "";
-  let domainNames = [];
+import "bootstrap";
+import "./style.css";
 
-  for (let i in pronoun) {
-    for (let j in adj) {
-      for (let k in noun) {
-        for (let l in end) {
-          domainName = [pronoun[i] + adj[j] + noun[k] + end[l]].join();
-          domainNames.push(domainName);
+window.onload;
+{
+  let pronoun = ["the", "our", "your", "my"];
+  let adj = ["great", "wonderful", "silly", "superb"];
+  let noun = ["goose", "tardigrade", "penguin", "clown"];
+  let end = [".com", ".io", ".org", ".net\n"];
+  let domainName = "";
+  let domainNames = "";
+
+  for (let i of pronoun) {
+    for (let j of adj) {
+      for (let k of noun) {
+        for (let l of end) {
+          domainName = `${i}${j}${k}${l}\n`;
+          domainNames += domainName;
         }
       }
     }
   }
   console.log(domainNames);
+  document.getElementById("body-p").innerHTML = domainNames;
+}
